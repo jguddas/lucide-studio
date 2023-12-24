fd8c47bd20b646b5f125efc42889f6e9a5a797f9
Jakob Guddas <jakob.guddas@lego.com>
2023-12-24 11:11:39 +0100

    feat: misc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lucide Studio",
+  title: "Lucide | Lucide Studio",
   description: "Edit and create lucide icons",
 };
 
