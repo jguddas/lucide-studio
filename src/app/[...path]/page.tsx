dbc059873c637c509f7f35ac6ddbc27dbacb03d4
Jakob Guddas <github@jguddas.de>
2024-03-02 22:33:29 +0100

    chore: testing out url based og image

---
+++ src/app/[...path]/page.tsx
@@ -0,0 +1,25 @@
+import PageClient from "../page-client";
+
+export async function generateMetadata({ params, searchParams }: any) {
+  const queryParams = new URLSearchParams(searchParams);
+  const url = `https://lucide-studio.vercel.app/api/og?${queryParams}`;
+
+  return {
+    title: "Lucide Studio",
+    description: "Edit and create lucide icons",
+    openGraph: {
+      images: [
+        {
+          url,
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+    twitter: { card: "summary_large_image", images: [{ url }] },
+  };
+}
+
+export default function Home() {
+  return <PageClient />;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,10 @@
+import { redirect } from "next/navigation";
 import PageClient from "./page-client";
 
-export async function generateMetadata({ params, searchParams }: any) {
+export default async function Home({ searchParams }: any) {
   const queryParams = new URLSearchParams(searchParams);
-  const url = `https://lucide-studio.vercel.app/api/og?${queryParams}`;
-
-  return {
-    title: "Lucide Studio",
-    description: "Edit and create lucide icons",
-    openGraph: {
-      images: [
-        {
-          url,
-          width: 1200,
-          height: 630,
-        },
-      ],
-    },
-    twitter: { card: "summary_large_image", images: [{ url }] },
-  };
-}
-
-export default function Home() {
+  if (queryParams.toString()) {
+    redirect(`/edit?${queryParams.toString()}`);
+  }
   return <PageClient />;
 }
