a0d05bc87100119e6ae73af627d73bea743bde04
Jakob Guddas <github@jguddas.de>
2024-03-02 22:05:36 +0100

    fix: og url base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,13 @@ import PageClient from "./page-client";
 
 export async function generateMetadata({ params, searchParams }: any) {
   const queryParams = new URLSearchParams(searchParams);
+  const url = `https://lucide-studio.vercel.app/api/og?${queryParams}`;
 
   return {
     title: "Lucide Studio",
     description: "Edit and create lucide icons",
-    openGraph: {
-      images: [{ url: `/api/og?${queryParams}` }],
-    },
-    twitter: {
-      card: "summary_large_image",
-      images: [{ url: `/api/og?${queryParams}` }],
-    },
+    openGraph: { images: [{ url }] },
+    twitter: { card: "summary_large_image", images: [{ url }] },
   };
 }
 
