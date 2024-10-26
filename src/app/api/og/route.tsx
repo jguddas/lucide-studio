5d59397510866ad3da6a72b4ca8136fdfdb22a22
Jakob Guddas <github@jguddas.de>
2024-10-26 15:10:36 +0200

    feat: added og image support for different icon sizes besides 24x24

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -25,6 +25,12 @@ export async function GET(request: Request) {
       `<path d="M14 12C14 9.79086 12.2091 8 10 8C7.79086 8 6 9.79086 6 12C6 16.4183 9.58172 20 14 20C18.4183 20 22 16.4183 22 12C22 8.446 20.455 5.25285 18 3.05557" />
     <path d="M10 12C10 14.2091 11.7909 16 14 16C16.2091 16 18 14.2091 18 12C18 7.58172 14.4183 4 10 4C5.58172 4 2 7.58172 2 12C2 15.5841 3.57127 18.8012 6.06253 21" />`,
   );
+  const width = parseInt(
+    searchParams.get("value")?.match(/width="(\d+)"/)?.[1] ?? "24",
+  );
+  const height = parseInt(
+    searchParams.get("value")?.match(/height="(\d+)"/)?.[1] ?? "24",
+  );
   let idx = 0;
   return new ImageResponse(
     (
@@ -43,11 +49,14 @@ export async function GET(request: Request) {
         }}
       >
         <svg
-          style={{ width: 400, height: 400 }}
+          style={{
+            width: 400 * (width / height > 1 ? 1 : width / height),
+            height: 400 * (height / width > 1 ? 1 : height / width),
+          }}
           xmlns="http://www.w3.org/2000/svg"
-          width={24}
-          height={24}
-          viewBox="0 0 24 24"
+          width={width}
+          height={height}
+          viewBox={`0 0 ${width} ${height}`}
           fill="none"
           stroke="currentColor"
           strokeWidth={2}
