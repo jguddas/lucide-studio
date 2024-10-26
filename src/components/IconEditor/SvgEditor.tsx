1f3d6ed856deb8b5b679aa6c2a3d5bbec7afbf13
Jakob Guddas <github@jguddas.de>
2024-10-26 14:50:13 +0200

    fix: resolved viewport issue

--- src/components/IconEditor/SvgEditor.tsx
+++ src/components/IconEditor/SvgEditor.tsx
@@ -15,7 +15,7 @@ const nodesToSvg = (nodes: INode[], height: number, width: number) => `<svg
   xmlns="http://www.w3.org/2000/svg"
   width="${width}"
   height="${height}"
-  viewBox="0 0 ${height} ${width}"
+  viewBox="0 0 ${width} ${height}"
   fill="none"
   stroke="currentColor"
   stroke-width="2"
